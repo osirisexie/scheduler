0e5e938833f4eaaa43ca8ced29bfc2f90c3555ce
Qianmo Xie <qianmoxie@Qianmos-MacBook-Pro.local>
2017-01-23 17:35:09 -0500

    oop shit

--- app.js
+++ app.js
@@ -5,159 +5,25 @@
 var app = angular.module('myApp', []);
 
 app.controller('schedulerController',schedulerController);
-function schedulerController ($scope){
+function schedulerController ($scope,$timeout){
 
     let scheduler = xqmScheduler.init();
-
-    $scope.days = [1,2,3,4,5,6,7];
-    $scope.hours = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
-    $scope.events = [{
-        id:'001',
-        index: [3,4,13]
-    },{
-        id:'002',
-        index: [5,12,16]
-    }];
-
-    let num_days = 7,
-        padding = 6,
-        num_hours = 28,
-        parent = $('.scheduler-wrapper'),
-        wrapper = $('.scheduler-scroll-pen'),
-        control = $('.scheduler-controls'),
-        w_height = wrapper.height(),
-        w_width = wrapper.width(),
-        u_width = (w_width +1)/7,
-        u_height = (w_height + 1)/28,
-        new_div = $(document.createElement('div'));
-
-    let template = new_div.clone().addClass('scheduler-events');
-    new_div.clone().addClass('scheduler-dragableBottom').appendTo(template);
-
-    control.mousedown(_create_event);
-
-    function _create_event (e){
-        let x = Math.ceil((e.pageX - parent.offset().left) * num_days / w_width);
-        let y = Math.ceil((e.pageY - parent.offset().top + parent.scrollTop()) * num_hours / w_height);
-        let div = template.clone();
-        div.css({
-            width:(u_width - padding)+'px',
-            height:(u_height - padding)+'px',
-            left:(u_width * (x-1))+'px',
-            top:(u_height * (y-1)) + 'px'
-        })
-        control.append(div);
-        control.mousemove(_init_move(div,x,y))
-        control.mouseup(_init_destory(div))
-    }
-
-    function _init_move (div,x,y){
-        return e=>{
-            let x2 = Math.ceil((e.pageX - parent.offset().left) * num_days / w_width);
-            let y2 = Math.ceil((e.pageY - parent.offset().top + parent.scrollTop()) * num_hours / w_height);
-            div.css({
-                width:(Math.abs(x2-x) * u_width+u_width - padding)+'px',
-                height:((Math.abs(y2-y)) * u_height + u_height - padding)+'px',
-                left:(u_width * (Math.min(x,x2)-1))+'px',
-                top:(u_height * (Math.min(y,y2)-1)) + 'px'
-            })
+    scheduler.create_event({
+        type:'cords',
+        cords:{
+            x:2,
+            y:12,
         }
+    })
+    let scheduler2 = xqmScheduler.init({
+        id:'xqm_scheduler2'
+    })
+
+    $scope.get=function(){
+        console.log(scheduler.get_events())
     }
-
-    function _init_destory(div){
-        return ()=>{
-            let dragable = div.find('.scheduler-dragableBottom');
-
-            dragable.mousedown(_resize_event(div))
-
-            div.mousedown(_drag_event);
-            control.unbind('mousemove');
-            control.unbind('mouseup');
-        }
-    }
-
-    function _drag_event(e){
-        e.stopPropagation();
-        e.preventDefault();
-        let x = e.pageX,y= e.pageY,target = $(e.target),
-          o_x = target.offset().left-wrapper.offset().left,
-          o_y = target.offset().top-wrapper.offset().top,
-          temp = target.clone();
-        target.css({
-            transition: '0s'
-        })
-        temp.css({
-            opacity: '0.3'
-        })
-        temp.addClass('scheduler-temp-node')
-        temp.appendTo(control);
-        control.mousemove(_drag_move(target, x, y, o_x, o_y));
-        control.mouseup(_drag_destory(target,temp));
-    }
-
-    function _drag_destory (target,temp){
-        return e=>{
-            target.css({
-                transition: '0.2s'
-            })
-            let x =Math.floor((target.offset().left-wrapper.offset().left)/u_width + 0.5),
-              y= Math.floor((target.offset().top-wrapper.offset().top)/u_height + 0.5);
-            target.css({
-                left:x*u_width+'px',
-                top:y*u_height+'px'
-            });
-            temp.remove();
-            control.unbind('mousemove');
-            control.unbind('mouseup');
-        }
-    }
-
-    function _drag_move (target, x, y, o_x, o_y){
-        return e=>{
-            let d_x = (e.pageX - x), d_y = (e.pageY - y);
-            target.css({
-                left:o_x+d_x+'px',
-                top:o_y+d_y+'px'
-            })
-        }
-    }
-
-    function _resize_event (div){
-        return e=>{
-            let x1 = div.offset().left,
-                x2 = x1 + div.width(),
-                y1 = div.offset().top - wrapper.offset().top,
-                n_y1 = Math.floor(y1/u_height+0.5);
-            e.stopPropagation();
-            e.preventDefault();
-            control.mousemove(_resize_move(div,x1,x2,y1,n_y1));
-            control.mouseup(_resize_destory());
-        }
-    }
-
-    function _resize_move(div,x1,x2,y1,n_y1){
-        return e=>{
-            let y = e.pageY + parent.scrollTop() - parent.offset().top,
-                n = Math.max(Math.floor(Math.abs(y-y1)/u_height + 0.5),1);
-            if(y < y1 - u_height/2){
-                div.css({
-                    height:(u_height * n - padding)+'px',
-                    top:(u_height * (n_y1-n)) + 'px'
-                })
-            }else{
-                div.css({
-                    height:(u_height * n - padding)+'px',
-                    top: n_y1 * u_height + 'px'
-                })
-            }
-        }
-    }
-
-    function _resize_destory(){
-        return e=>{
-            control.unbind('mousemove');
-            control.unbind('mouseup');
-        }
+    $scope.get2=function(){
+        console.log(scheduler2.get_events())
     }
 
 
--- scheduler.js
+++ scheduler.js
@@ -3,146 +3,351 @@
 })(function(){
   var options = {
     id:'xqm_scheduler',
-    w_width: 700,
-    w_height: 700,
-    i_height: 1400,
+    w_width: 500,
+    w_height: 600,
+    i_height: 1000,
   };
+  let total_wrapper_id = 'xqmScheduler-wrapper', mouse_wrapper_id = 'xqmScheduler-content';
 
   function get_cords(e){
-    let parent;
-    if(this.el.hasClass('xqmScheduler-wrapper')){
-      parent = this.el;
+    let c_el;
+    if(this.el.hasClass(total_wrapper_id)){
+      c_el = this.el.find(`.${mouse_wrapper_id}`);
     } else{
-      parent = this.el.closest('.xqmScheduler-wrapper')
+      c_el = this.el.closest(`.${mouse_wrapper_id}`);
     }
-    return {
-      x:e.pageX - parent.offset().left,
-      y:e.pageY - parent.offset().top,
+    let cords = {
+      x:e.pageX - c_el.offset().left,
+      y:e.pageY - c_el.offset().top,
+    }
+    return  {
+        x: Math.floor(cords.x / this.config.m_w ),
+        y: Math.floor(cords.y / this.config.m_h ),
     }
   }
 
-  function get_rect(x1,y1,x2,y2,m_w,m_h){
-
-    return [x2>=x1?x1:Math.min(x1-m_w,x2),y2>=y1?y1:Math.min(y1-m_h,y2),Math.max(m_w,Math.abs(x1-x2)),Math.max(Math.abs(y1-y2),m_h)];
+  function get_rect(x1,y1,x2,y2){
+    return [x1,Math.min(y1,y2),1,Math.max(Math.abs(y1-y2),1)];
   }
 
-  function calc_event_rect(e){
+  function calc_event_rect(e, old_cords){
+    if(!old_cords){
+      old_cords = {
+          x:this.data().cords[0],
+          y:this.data().cords[1]
+      }
+    }
     let new_cords = get_cords.call(this,e);
-    let data = this.data();
-    let old_cords = {
-      x:data.cords[0],
-      y:data.cords[1]
-    }
-    return get_rect(old_cords.x,old_cords.y,new_cords.x,new_cords.y,this.config.m_w,this.config.m_h);
+    return get_rect(old_cords.x,old_cords.y,new_cords.x,new_cords.y);
   }
 
-  class mouseDown{
+  function to_time_string (num){
+    let m = num % 60;
+    m = m ==0?'00':m;
+    let h = Math.floor(num/60);
+    return `${h}:${m}`;
+  }
 
-    bind_mouse_down(){
-      this.el.on('mousedown', this.mouse_down.bind(this))
-    }
-
-    mouse_down(e){
-      return get_cords.call(this, e);
-    }
+  function remove_event_helper(list,event){
+    let index = list.indexOf(event);
+    list.splice(index,1);
 
   }
 
-  class Event extends mouseDown{
+  let event_builder ={
+
+      build: function(){
+          event_builder.add_resizer.call(this,'top');
+          event_builder.add_resizer.call(this,'bottom');
+          event_builder.add_remover.call(this);
+          event_builder.add_copier.call(this);
+      },
+
+      add_resizer : function(position){
+          let el = $(`<div class="xqmScheduler-resizer-${position}">`).appendTo(this.el);
+          el.css({
+              height:`${this.config.m_h/15}px`
+          })
+          el.on('mousedown', event_mouse_event_handler.hit_resizer(position, this))
+      },
+
+      add_remover: function(){
+          let el = $(`<i class="xqmScheduler-remover fa fa-trash-o"></i>`).appendTo(this.el);
+          el.on('click',this.destory.bind(this))
+      },
+
+      add_copier: function(){
+          let el = $('<i class="xqmScheduler-copier fa fa-clone"></i>').appendTo(this.el);
+          el.on('click',this.copy.bind(this))
+      }
+
+  };
+
+  let scheduler_builder = {
+
+    build:function(options){
+        scheduler_builder.build_main.call(this,options);
+        scheduler_builder.build_content.call(this,options);
+        scheduler_builder.build_scales.call(this,options);
+
+    },
+
+    build_main:function(options){
+        let main = $('#'+options.id);
+        main.css({
+            height: `${options.w_height}px`,
+            width: `${options.w_width}px`,
+
+        }).addClass('xqmScheduler-wrapper');
+        this.el = main;
+
+    },
+
+    build_content:function(options){
+        let content = $('<div class="xqmScheduler-content" />');
+        content.css({
+            left:'0',
+            right:'0',
+            top:'0',
+            height:`${options.i_height}px`,
+        }).appendTo(this.el);
+
+        this.content = content;
+    },
+
+    build_scales:function(options){
+        let hori = $('<div class="xqmScheduler-hori-wrapper" />').appendTo(this.content);
+        for(let i = 0; i<24; i++){
+            $('<div class="xqmScheduler-hori" />').appendTo(hori);
+        }
+
+        let vert = $('<div class="xqmScheduler-vert-wrapper" />').appendTo(this.content);
+        for(let i = 0; i<7; i++){
+            $('<div class="xqmScheduler-vert" />').appendTo(vert);
+        }
+    }
+  }
+
+  let mouse_event_handler = {
+
+      mouse_event:function(e){
+          return get_cords.call(this,e);
+      },
+      stop_further:function(e){
+          e.preventDefault();
+          e.stopPropagation();
+      }
+  };
+
+  let event_mouse_event_handler = Object.create(mouse_event_handler);
+  Object.assign(event_mouse_event_handler,{
+
+      start_move:function(event){
+          return e=>{
+              this.stop_further(e);
+              let cords = this.mouse_event.call(event,e);
+              event_listerner_handler.startListen.call(event,'reposition',cords);
+          }
+
+      },
+
+      mouse_move_reposition:function(){
+          let event = arguments[0], o_cords = arguments[2];
+          return e=>{
+              let cords = this.mouse_event.call(event,e);
+              event.re_render(event.el.data('s-data').cords[0] + cords.x - o_cords.x,event.el.data('s-data').cords[1] + cords.y - o_cords.y,event.el.data('s-data').cords[2],event.el.data('s-data').cords[3])
+          }
+      },
+
+      mouse_move_reposition_stop:function(){
+          let e = arguments[0], event = arguments[1], o_cords = arguments[3];
+          let cords = this.mouse_event.call(event,e);
+          let data = event.data();
+          data.cords = [event.el.data('s-data').cords[0] + cords.x - o_cords.x,event.el.data('s-data').cords[1] + cords.y - o_cords.y,event.el.data('s-data').cords[2],event.el.data('s-data').cords[3]]
+          event.data(data);
+
+      },
+
+      mouse_move_resize: function(){
+          let event = arguments[0];
+          let o_cords;
+          if(arguments.length >= 3) o_cords = arguments[2];
+          return e=>{
+              this.resize = true;
+              let rect = calc_event_rect.call(event,e,o_cords);
+              event.re_render(...rect);
+          }
+      },
+
+      mouse_move_resize_stop: function(e){
+          if(this.resize){
+              let e = arguments[0], event = arguments[1], o_cords;
+              if(arguments.length >= 4) o_cords = arguments[3];
+              let rect = calc_event_rect.call(event,e,o_cords);
+              let data = event.data();
+              data.cords = rect;
+              event.data(data);
+          }
+          this.resize = false;
+
+      },
+
+      hit_resizer:function(position,event){
+          return e=>{
+              this.stop_further(e);
+              let cords= {
+                  x:event.data().cords[0],
+                  y:event.data().cords[1]
+              };
+              if(position == 'top'){
+                  cords.y = event.data().cords[1]+event.data().cords[3]
+              }
+              event_listerner_handler.startListen.call(event,'resize',cords);
+          }
+      }
+
+
+  });
+
+  let scheduler_mouse_event_handler = Object.create(mouse_event_handler);
+  Object.assign(scheduler_mouse_event_handler,{
+      mouse_down:function(scheduler){
+          return e=>{
+              let event = scheduler.create_event({
+                  type:'cords',
+                  cords: this.mouse_event.call(scheduler,e),
+              });
+              event_listerner_handler.startListen.call(event,'resize')
+          }
+
+      }
+  })
+
+  let event_listerner_handler = {
+      startListen:function(name){
+          let mouse_wrapper = this.el.closest('.xqmScheduler-wrapper').find(`.${mouse_wrapper_id}`);
+          mouse_wrapper.on('mousemove',event_mouse_event_handler[`mouse_move_${name}`](this,...arguments))
+          mouse_wrapper.on('mouseup', e=>{
+              mouse_wrapper.unbind('mousemove');
+              mouse_wrapper.unbind('mouseup');
+              event_mouse_event_handler[`mouse_move_${name}_stop`](e,this,...arguments);
+          });
+      }
+  }
+
+  class Event{
+    
     constructor(options){
-      super()
       if(options.type == 'cords'){
         this.config={
           m_w:options.m_w,
-          m_h:options.m_h
-        }
-        let event = {
-          cords:[options.cords.x, options.cords.y, 30, 30]
-        }
-        this.el = $('<div class="xqmScheduler-event"/>').css({
-          top:`${options.cords.y}px`,
-          left:`${options.cords.x}px`,
-          width:'50px',
-          height:'50px',
-        })
-        this.el.data('s-data',event);
+          m_h:options.m_h,
+        };
+        let data = {
+          cords:[options.cords.x, options.cords.y, 1, options.cords.h?options.cords.h:2],
+        };
+        this.el = $('<div class="xqmScheduler-event"/>');
+        this.data(data);
+        event_builder.build.call(this);
+        this.re_render(...data.cords);
+        this.el.on('mousedown', event_mouse_event_handler.start_move(this));
       }
     }
 
-    resize(x,y,w,h){
+    setid(id){
+      this.id = id;
+      let data = this.data();
+      this.data(data);
+    }
+
+    destory(){
+      this.parent.remove_event(this);
+    }
+
+    copy(){
+      this.parent.copy_event(this);
+    }
+
+    re_render(x,y,w,h){
       this.el.css({
-        top:`${y}px`,
-        left:`${x}px`,
-        width:`${w}px`,
-        height:`${h}px`,
-      })
+        top:`${y * this.config.m_h}px`,
+        left:`${x * this.config.m_w}px`,
+        width:`${w * this.config.m_w}px`,
+        height:`${h * this.config.m_h}px`,
+      });
+      this.parse_data();
     }
 
-    mouse_down(e){
-      e.preventDefault()
-      this.startListen();
-    }
-
-    startListen(){
-      let mouse_wrapper = this.el.closest('.xqmScheduler-wrapper').find('.xqmScheduler-mouse');
-      mouse_wrapper.on('mousemove',this.mouse_move.bind(this))
-      mouse_wrapper.on('mouseup', this.stopListen.bind(this,mouse_wrapper));
-    }
-
-    data(){
+    data(data){
+      if(data) this.el.data('s-data',data);
       return this.el.data('s-data');
     }
 
-    mouse_move(e){
-      let rect = calc_event_rect.call(this,e);
-      this.resize(...rect);
+    parse_data(){
+      let data = this.data().cords;
+      this.week = data[0];
+      this.start = data[1]*30;
+      this.end = this.start + data[3]*30;
+      this.start = to_time_string(this.start);
+      this.end = to_time_string(this.end);
     }
 
-    stopListen(other,e){
-      other.unbind('mousemove');
-      other.unbind('mouseup');
-      let rect = calc_event_rect.call(this,e);
-      let data = this.data();
-      data.cords = rect;
-      this.el.data('s-data',event);
-    }
 
   }
 
-  class scheduler extends mouseDown{
+  class scheduler{
+    
     constructor(cus_opt){
-      super();
+      this.events= [];
+      this.eventid = 0;
       options = Object.assign(options,cus_opt);
-      this.options = options;
-      let main = $('#'+options.id);
-      main.css({
-        border: '1px solid grey',
-        height: `${options.w_height}px`,
-        width: `${options.w_width}px`,
-        'overflow-y': 'scroll',
-        position: 'relative'
-      });
-      main.addClass('xqmScheduler-wrapper');
-      $('<div class="xqmScheduler-mouse"></div>').appendTo(main);
-      this.el= main;
-      this.bind_mouse_down();
+      scheduler_builder.build.call(this,options);
+      this.content.on('mousedown',scheduler_mouse_event_handler.mouse_down(this));
+      this.config = Object.assign(options,{
+          m_w:(this.content.width()+1)/7,
+          m_h:(this.content.height()+1)/48
+      })
+
     }
 
     create_event(options){
+      options = Object.assign({
+          m_w:this.config.m_w,
+          m_h:this.config.m_h,
+      },options)
       let event = new Event(options);
-      event.el.appendTo(this.el)
+      event.setid(this.eventid++);
+      event.parent = this;
+      event.el.appendTo(this.content)
+      this.events.push(event);
       return event;
     }
 
-    mouse_down(e){
-      this.create_event({
-        type:'cords',
-        cords: super.mouse_down(e),
-        m_w:this.options.w_width/7,
-        m_h:this.options.i_height/48
-      }).startListen()
+    remove_event(event){
+      event.el.remove();
+      remove_event_helper(this.events,event);
     }
 
+    copy_event(event){
+      let cords = event.data().cords;
+      let new_event = this.create_event({
+        type:'cords',
+        cords:{
+          x: cords[0],
+          y: cords[1],
+          h: cords[3]
+        }
+      });
+      new_event.re_render(cords[0]*1.01,cords[1]*1.01, cords[2],cords[3]);
+      return new_event;
+    }
+
+    get_events(){
+      this.events.forEach(event=>{
+        event.parse_data();
+      })
+      return this.events;
+    }
   }
 
   return {
